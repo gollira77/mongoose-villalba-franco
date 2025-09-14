2c3cda65f7b51ddd8599baf81a0a62ebd6e21c2c
Franco <franquivillalba13@gmail.com>
2025-09-14 19:05:13 -0300

    Conexión a MongoDB fija y configuración básica de app.js

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 import express from "express"; 
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import "dotenv/config";
+import { connectDB } from "./src/config/database.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+// Middlewares 
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.listen(PORT, async () => {
+    await connectDB();
     console.log(`Servidor escuchando el el puerto ${PORT}`);
 })
